b0a314e2077943889673c0104d3fb25d0bf85b84
llyybbb <yb9766@naver.com>
2026-01-22 19:24:38 +0900

    style: 리퀴드 글래스 스타일 완성

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import * as THREE from 'three'
-import { Canvas } from '@react-three/fiber'
-import FullscreenBackground from './components/FullscreenBackground'
-import LiquidGlassCard from './components/LiquidGlassCard'
-import LiquidInfoBox from './components/LiquidInfoBox'
-// import ExhibitionHall from './pages/ExhibitionHall'
+import ExhibitionHall from './pages/ExhibitionHall'
+
 
 export default function App() {
   const queryClient = new QueryClient()
@@ -13,15 +9,7 @@ export default function App() {
       <div className="w-screen h-screen bg-cover bg-center">
         <QueryClientProvider client={queryClient}>
           <div className="w-screen h-screen bg-cover bg-center">
-                <Canvas
-                  className="fixed inset-0 w-screen h-screen"
-                  camera={{ position: [0, 0, 5], fov: 50 }}
-                  gl={{ toneMapping: THREE.ACESFilmicToneMapping }}
-                >
-                  <FullscreenBackground />
-                  <LiquidGlassCard />
-                  <LiquidInfoBox />
-                </Canvas>
+            <ExhibitionHall />
               </div>
         </QueryClientProvider>
       </div>
--- src/pages/ExhibitionHall.tsx
+++ src/pages/ExhibitionHall.tsx
@@ -1,69 +1,17 @@
-import { useEffect, useRef, useState } from 'react'
 import '../styles/glass.css'
 
 export default function ExhibitionHall () {
-    const [position, setPosition] = useState({ x: 100, y: 100 })
-    const [dragging, setDragging] = useState(false)
-    const dragStart = useRef({ x: 0, y: 0 })
-
-    const handleMouseDown = (e) => {
-      setDragging(true)
-      dragStart.current = {
-        x: e.clientX - position.x,
-        y: e.clientY - position.y,
-      }
-    }
-
-    const handleMouseMove = (e) => {
-      if (!dragging) return
-      setPosition({
-        x: e.clientX - dragStart.current.x,
-        y: e.clientY - dragStart.current.y,
-      })
-    }
-
-    const handleMouseUp = () => {
-      setDragging(false)
-    }
-
-    useEffect(() => {
-      window.addEventListener('mousemove', handleMouseMove)
-      window.addEventListener('mouseup', handleMouseUp)
-      return () => {
-        window.removeEventListener('mousemove', handleMouseMove)
-        window.removeEventListener('mouseup', handleMouseUp)
-      }
-    }, [dragging])
+   
   return (
     <>
-      <svg style={{ display: 'none' }}>
-        <filter id="liquid-glass">
-          <feImage
-            href="./red_green_gradient_border.png"
-            preserveAspectRatio="none"
-          />
-
-          <feDisplacementMap
-            in="SourceGraphic"
-            scale="0"
-            xChannelSelector="R"
-            yChannelSelector="G"
-          />
-        </filter>
-      </svg>
       <div
         style={{ backgroundImage: `url('/exhibitionBg.svg')` }}
-        className="h-[300%] w-screen bg-cover bg-center "
+        className="h-screen w-screen bg-cover bg-center "
       >
         <div className="h-screen w-screen bg-black/25 backdrop-blur-[3px]">
           <div className="h-full w-full flex flex-col items-center justify-center">
             <div
-              onMouseDown={handleMouseDown}
-              className="w-[500px] h-[80px] card cursor-move absolute"
-              style={{
-                left: `${position.x}px`,
-                top: `${position.y}px`,
-              }}
+              className="w-[500px] h-[80px] glass"
             ></div>
             
           </div>
